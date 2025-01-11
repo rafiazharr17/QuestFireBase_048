59a14107b11a843c1dd02deae58cc6dd4cf3ad41
rafiazharr17 <rafipilo17@gmail.com>
2025-01-11 21:37:14 +0700

    membuat fungsi untuk menyimpan input form kedalam entity Mahasiswa

--- app/src/main/java/com/rafi/pertemuan12/ui/insert/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/rafi/pertemuan12/ui/insert/viewmodel/InsertViewModel.kt
@@ -1,5 +1,7 @@
 package com.rafi.pertemuan12.ui.insert.viewmodel
 
+import com.rafi.pertemuan12.model.Mahasiswa
+
 sealed class FormState {
     object Idle : FormState()
     object Loading : FormState()
@@ -14,4 +16,13 @@ data class MahasiswaEvent(
     val alamat: String = "",
     val kelas: String = "",
     val angkatan: String = "",
+)
+
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jenisKelamin = jenisKelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
 )
