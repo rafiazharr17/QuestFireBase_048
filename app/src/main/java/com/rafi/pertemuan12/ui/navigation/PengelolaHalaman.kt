253df8929a95e0c1a0e5e0d6052f80e818e3770a
rafiazharr17 <rafipilo17@gmail.com>
2025-01-11 23:20:40 +0700

    membuat fungsi PengelolaHalaman

---
+++ app/src/main/java/com/rafi/pertemuan12/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,37 @@
+package com.rafi.pertemuan12.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.rafi.pertemuan12.ui.home.pages.HomeScreen
+import com.rafi.pertemuan12.ui.insert.pages.InsertMhsView
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                }
+            )
+        }
+
+        composable(DestinasiInsert.route){
+            InsertMhsView(
+                onBack = { navController.popBackStack() },
+                onNavigate = { navController.navigate(DestinasiHome.route) }
+            )
+        }
+    }
+}
