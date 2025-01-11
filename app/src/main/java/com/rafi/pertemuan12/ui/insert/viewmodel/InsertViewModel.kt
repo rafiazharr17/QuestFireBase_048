7dcce12cd56c8b10b1dd061415fd8bac75efa5a6
rafiazharr17 <rafipilo17@gmail.com>
2025-01-11 21:33:27 +0700

    membuat sealed class FormState

---
+++ app/src/main/java/com/rafi/pertemuan12/ui/insert/viewmodel/InsertViewModel.kt
@@ -0,0 +1,8 @@
+package com.rafi.pertemuan12.ui.insert.viewmodel
+
+sealed class FormState {
+    object Idle : FormState()
+    object Loading : FormState()
+    data class Success(val message: String) : FormState()
+    data class Error(val message: String) : FormState()
+}
